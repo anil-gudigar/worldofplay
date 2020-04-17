fcab97b8d15bf538594f4f581e68bd5bb7a00fb2
anilkumargudigar <anil.kumar@daytoday.health>
2020-04-17 11:40:56 +0530

    themes support added.

--- app/src/main/java/com/worldofplay/app/ui/MainActivity.kt
+++ app/src/main/java/com/worldofplay/app/ui/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.worldofplay.app.R
 import com.worldofplay.app.stylekit.themes.view.ThemesActivity
 import dagger.android.DispatchingAndroidInjector
@@ -22,11 +20,9 @@ class MainActivity : ThemesActivity(),HasAndroidInjector{
     override val layout: Int = R.layout.activity_main
 
     override fun initUI() {
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_topStories))
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 
     override fun androidInjector() = dispatchingAndroidInjector
